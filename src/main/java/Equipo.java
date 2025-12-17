250aef3b9ae9b5ff26c4ab2b87e4605f64226eef
Merino101010 <amerinocas@educacion.navarra.es>
2025-12-17 13:01:36 +0100

    Clase proyecto terminada y modificada clase equipo con arrays

--- src/main/java/Equipo.java
+++ src/main/java/Equipo.java
@@ -15,20 +15,13 @@ public class Equipo {
     }
 
     public Trabajador getParticipante(int numero) {
-        switch (numero) {
-            case 1: return participante1;
-            case 2: return participante2;
-            case 3: return participante3;
-        }
-        return null;
+
+        return participantes[numero-1];
     }
 
     public void setParticipante(int numero, Trabajador trabajador) {
-        switch (numero) {
-            case 1: participante1 = trabajador; break;
-            case 2: participante2 = trabajador; break;
-            case 3: participante3 = trabajador; break;
-        }
+
+        participantes[numero-1] = trabajador;
     }
 //Alvaro
     public boolean tieneLider() {
